b2b970f1673999bec67c9f483cc6d1c27d6212b7
letcharren <leo_etcharren@hotmail.com>
2019-03-25 17:23:02 -0300

    Agregado comentarios a funciones en librerias

--- POWERDEVS/atomicosproyecto/NumberGenLibrary.h
+++ POWERDEVS/atomicosproyecto/NumberGenLibrary.h
@@ -1,16 +1,24 @@
-//#include <iostream>
-//#include <list>
 #include <random>
 #include <algorithm>
-//#include "simulator.h"
-//#include "event.h"
-//#include "stdarg.h"
 #include "Library.h"
 
+//Genera una lista de reales con distribucion Exponencial
 std::list<double> genExponentialDistribution(double mean, int qty,unsigned int seed);
+
+//Genera una lista de reales con distribucion Uniforme
 std::list<double> genUniformDistribution(double min, double max, int qty,unsigned int seed);
+
+//Ordena la lista de forma intercalada
 void interleaved(std::list<double>* a);
+
+//Saca un elemento random de la lista
 double popRandomElement(std::list<double>* a);
+
+//Saca el mayor elemento de la lista
 double popWeightMayorPcElem(std::list<double>* weights,double enemyWeight);
+
+//Saca el elemento que puede ganarle al peso enemyWeight segun la distancia que este recorrio y el tiempo en que se producira la proxima colision
 double pickPossibleWinner(std::list<double>* weights, double enemyWeight, double enemyDistance, double length );
+
+//Cheque si hay pesos que puedan ganarle a enemyWeight
 bool checkPossibleWinner(std::list<double> weights, double enemyWeight, double enemyDistance, double length );
