f9e030a1c678443e942062e1d5499c2880047253
valentin vivaldi <valenvivaldi@gmail.com>
2019-03-26 22:22:39 -0300

    comentarios librerias

--- POWERDEVS/atomicosproyecto/Library.h
+++ POWERDEVS/atomicosproyecto/Library.h
@@ -8,28 +8,60 @@
 #include "event.h"
 #include "stdarg.h"
 
-//Calcula el tiempo en que se producira la proxima colision
+//Calcula el tiempo en que se producira la proxima colision,
+// devolviendo en cuanto tiempo van a colisionar los primeros elementos de ambas colas
+//teniendo en cuenta el largo de la cinta, y la velocidad
+//PARAMETROS:
+// playerQueue: cola del jugador human
+// pcQueue: cola del jugador pc
+//l: largo de la cola
+// time: tiempo en el que ocurre la colision
+// vc:velocidad de la cola
 double calculateSigma(std::list< std::pair<double,double> >playerQueue  , std::list< std::pair<double,double> > pcQueue , double time, double l, double vc);
 
-//Calcula el peso nuevo del Objeto vencedor
+//Calcula el peso nuevo del Objeto vencedor, a partir de los
+// PARAMETROS:
+// winnerQueue: cola del jugador que gano la colision
+// loserQueue: cola del jugador que perdio de la colisiones
+// time: tiempo en el que ocurre la colision
+// vc:velocidad de la cola
 double newWeight(std::list< std::pair<double,double> > winnerQueue , std::list< std::pair<double,double> > loserQueue , double time,double vc);
 
 //Calcula la potencia del primer elemento de una cola de cajas segun su tiempo de arribo, peso, tiempo de ejecucion y velocida de la Cinta
+// PARAMETROS:
+// queue: cola
+// time: tiempo
+// vc:velocidad de la cola
 double power(std::list< std::pair<double,double> > queue , double time,double vc);
 
 //Calcula la potencia de una caja segun su peso y distancia recorrida
+// PARAMETROS:
+// weigth: peso de la cajas
+// distance: distancia que lleva recorrida la caja
 double power(double weigth, double distance);
 
 //Calcula la distancia rrecorrida del primer elemento de la cola de cajas
+// PARAMETROS:
+// queue: cola del jugador
+// time: tiempo en el que ocurre la colision
+// vc:velocidad de la cola
 double dist(std::list< std::pair<double,double> > queue , double time,double vc);
 
 //Imprime los pesos de una cola de cajas
+// PARAMETROS:
+// queue: cola de pesos
+
 void showQueue (std::list< std::pair<double,double> > queue);
 
 //Devuelve el peso del segundo elemento de una cola de cajas
+// PARAMETROS:
+// queue: cola del jugador que gano la colision
 double weightSecondElement(std::list< std::pair<double,double> > queue);
 
 //Calcula la distancia en que se encuentra el segundo elemento de la cola de cajas
+// PARAMETROS:
+// queue: cola del jugador que gano la colision
+
 double distSecond(std::list< std::pair<double,double> > queue , double time,double vc);
 
 
--- POWERDEVS/atomicosproyecto/NumberGenLibrary.h
+++ POWERDEVS/atomicosproyecto/NumberGenLibrary.h
@@ -3,23 +3,44 @@
 #include "Library.h"
 
 //Genera una lista de reales con distribucion Exponencial
+// PARAMETROS:
+//mean: media de los valores generados
+//qty: cantidad de valores a generar
+//seed: semillas de la generacion(si es 0 genera una aleatoria)
 std::list<double> genExponentialDistribution(double mean, int qty,unsigned int seed);
 
 //Genera una lista de reales con distribucion Uniforme
+// PARAMETROS:
+// min: valor minimo de los valores a generar
+// max: valor maximo de los valores a generar
+//qty: cantidad de valores a generar
+//seed: semillas de la generacion(si es 0 genera una aleatoria)
 std::list<double> genUniformDistribution(double min, double max, int qty,unsigned int seed);
 
 //Ordena la lista de forma intercalada
+// PARAMETROS:
+//a: puntero a la lista
 void interleaved(std::list<double>* a);
 
-//Saca un elemento random de la lista
+//Saca un elemento random de la lista  de pesos y lo retorna
+// PARAMETROS:
+//a: puntero a la lista
 double popRandomElement(std::list<double>* a);
 
-//Saca el mayor elemento de la lista
+//Saca el minimo peso, de la lista, que supera a enemyWeight
 //Si no lo encuentra saca un elemento random
+// PARAMETROS:
+//weights: puntero a la lista de pesos
+//enemyWeight: peso de la caja que se quiere superar
 double popWeightMayorPcElem(std::list<double>* weights,double enemyWeight);
 
 //Saca el elemento que puede ganarle al peso enemyWeight segun la distancia que este recorrio y el tiempo en que se producira la proxima colision
 //Si no lo encuentra saca un elemento random
+// PARAMETROS:
+//weights: puntero a la lista
+//enemyWeight: peso de la caja que se quiere derrotar
+//enemyDistance: distancia que lleva recorrida el enemigo
+//length: largo total de la cinta
 double pickPossibleWinner(std::list<double>* weights, double enemyWeight, double enemyDistance, double length );
 
 //Cheque si hay pesos que puedan ganarle a enemyWeight
